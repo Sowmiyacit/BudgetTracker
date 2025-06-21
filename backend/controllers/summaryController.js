a8b0771fe1efbfd753f96b4de1091985e1d501a8
Sowmiya <sowmiyasuvaitha3434@gmail.com>
2025-06-21 21:43:13 +0530

    Update summaryController.js

--- backend/controllers/summaryController.js
+++ backend/controllers/summaryController.js
@@ -5,21 +5,21 @@ export const getSummary = async (req, res) => {
   try {
     const userId = req.user._id;
 
-    // Total Income
+  
     const incomeAgg = await Transaction.aggregate([
       { $match: { user: userId, type: 'income' } },
       { $group: { _id: null, total: { $sum: '$amount' } } }
     ]);
     const totalIncome = incomeAgg[0]?.total || 0;
 
-    // Total Expense
+   
     const expenseAgg = await Transaction.aggregate([
       { $match: { user: userId, type: 'expense' } },
       { $group: { _id: null, total: { $sum: '$amount' } } }
     ]);
     const totalExpense = expenseAgg[0]?.total || 0;
 
-    // Total Budget
+   
     const budgetAgg = await Budget.aggregate([
       { $match: { user: userId } },
       { $group: { _id: null, total: { $sum: '$amount' } } }
